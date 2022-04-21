aebcef06d4459f8d1fe2483beb2d64be9954408b
cesar jesus leon martin <cesilius98@gmail.com>
2022-04-21 11:51:20 +0200

    Feature: añadido insert y add

--- src/CustomCollection.java
+++ src/CustomCollection.java
@@ -5,4 +5,29 @@ public class CustomCollection {
     public CustomCollection(int initialCapacity){
         data = new Object[initialCapacity];
     }
+    public Object getElement(int position){
+        return data[position];
+    }
+    public int length(){
+        return length;
+    }
+    public void insert(Object element, int position){
+        if(length == data.length){
+            Object []aux = data;
+            data= new Object[data.length *2];
+
+            for (int i = 0; i < aux.length; i++) {
+                data[i] = aux[i];
+            }
+            aux= null; //esto es para que el recolector de basura se cargue a aux
+        }
+        for (int i = length -1; i>=position ;i--){
+            data[i+1]= data[i];
+        }
+        data[position]=element;
+        length++;
+    }
+    public void add(Object element){
+        insert(element,length);
+    }
 }
