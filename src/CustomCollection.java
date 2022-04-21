8bd4570b529c5d29f2bc07d0004b8d23d00ddc9b
cesar jesus leon martin <cesilius98@gmail.com>
2022-04-21 10:45:15 +0200

    Feature: Creada la clase Custom Collection

---
+++ src/CustomCollection.java
@@ -0,0 +1,8 @@
+public class CustomCollection {
+    private Object data[] = null;
+    private int length =0;
+
+    public CustomCollection(int initialCapacity){
+        data = new Object[initialCapacity];
+    }
+}
