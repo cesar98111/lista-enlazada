e9dc6f9784bf2a9b322f5907245e092741002eec
cesar jesus leon martin <cesilius98@gmail.com>
2022-04-22 15:12:16 +0200

    guardado

---
+++ src/com/company/GenericCollection.java
@@ -0,0 +1,65 @@
+package com.company;
+
+public class GenericCollection <T>{
+    private Object data [] = null;
+    private int length = 0;
+
+    public GenericCollection (int initialCapacity){
+        data = new Object[initialCapacity];
+    }
+    public T getElement(int position){
+        return (T)data[position];
+    }
+    public int Length() {return length;}
+    public void insert(T element, int position){
+        if(length == data.length){
+            Object []aux = data;
+            data= new Object[data.length *2];
+
+            for (int i = 0; i < aux.length; i++) {
+                data[i] = aux[i];
+            }
+            aux= null; //esto es para que el recolector de basura se cargue a aux
+        }
+        for (int i = length -1; i>=position ;i--){
+            data[i+1]= data[i];
+        }
+        data[position]=element;
+        length++;
+    }
+    public void add(T element){
+        insert(element,length);
+    }
+    public int search(T element){
+        for (int i = 0; i < length; i++) {
+            if(data[i].equals(element)){
+                return i;
+            }
+
+        }
+        return -1;
+    }
+    public Object delete(int position){
+        Object aux = data[position];
+
+        for(int i= position; i< length -1; i++){
+            data[i]=data[i+1];
+        }
+        data[length-1]=null;
+        length--;
+        return aux;
+    }
+
+    @Override
+    public String toString() {
+        if (length==0){
+            return "<Empty>";
+        }
+        String out="";
+        for (int i = 0; i < length; i++) {
+            out+=getElement(i)+",";
+        }
+        return out;
+
+    }
+}
